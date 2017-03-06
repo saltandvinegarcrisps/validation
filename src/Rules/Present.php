57b39888c3ef96667a00854043f6e836bff9ae26
Kieron <rwarasaurus@users.noreply.github.com>
2017-03-06 11:13:12 +0000

    Create Present.php

---
+++ src/Rules/Present.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Validation\Rules;
+
+class Present extends AbstractRule
+{
+    protected $message = '%s must be set';
+
+    public function isValid(): bool
+    {
+        $value = $this->getValue();
+
+        return null === $value;
+    }
+}
