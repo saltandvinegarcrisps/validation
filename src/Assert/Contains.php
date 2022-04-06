0ea62509692760f22039298c88f73c428c0493b0
Kieron <kieron@fastmail.fm>
2022-04-06 10:33:03 +0100

    updated phpstan

--- src/Assert/Contains.php
+++ src/Assert/Contains.php
@@ -28,12 +28,10 @@ class Contains extends Assertion implements Constraint
 
     public function isValid($values): bool
     {
-        $this->validated = is_array($values) && count($values) > 0;
-
-        if (!$this->validated) {
+        if (!is_array($values) || count($values) > 0) {
             return false;
         }
-
+        $this->validated = true;
         return array_reduce($values, function (bool $carry, $value) {
             return $this->constraint->isValid($value) ? $carry : false;
         }, true);
