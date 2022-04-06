bbf090033b5fdef0b88a48820a4d09d227c5d521
Kieron <kieron@fastmail.fm>
2022-04-06 10:42:35 +0100

    fixing tests

--- src/Assert/Contains.php
+++ src/Assert/Contains.php
@@ -26,14 +26,24 @@ class Contains extends Assertion implements Constraint
         return $this->constraint->getMessage($attribute);
     }
 
+    /**
+     * @param string|array|null $values
+     * @return array
+     */
+    protected function validate($values): array
+    {
+        vaR_dump($values);
+        if (is_array($values) && count($values) > 0) {
+            $this->validated = true;
+            return $values;
+        }
+        return [];
+    }
+
     public function isValid($values): bool
     {
-        if (!is_array($values) || count($values) > 0) {
-            return false;
-        }
-        $this->validated = true;
-        return array_reduce($values, function (bool $carry, $value) {
-            return $this->constraint->isValid($value) ? $carry : false;
-        }, true);
+        return array_reduce($this->validate($values), function (bool $carry, $value) {
+            return $this->constraint->isValid($value) ? true : $carry;
+        }, false);
     }
 }
